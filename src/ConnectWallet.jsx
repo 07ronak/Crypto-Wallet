c11cced5108ed0e89abb5e285a0aa67a7fdb5062
07ronak <ronakhingonia123456789@gmail.com>
2024-08-24 02:01:07 +0530

    Added comments and alerts

--- src/ConnectWallet.jsx
+++ src/ConnectWallet.jsx
@@ -5,30 +5,41 @@ const ConnectWallet = () => {
   const [balance, setBalance] = useState(null);
 
   const connectMetamask = async () => {
+    // Check if Metamask is installed in the browser
     if (window.ethereum) {
       try {
+        // Request access to the user's Ethereum accounts
         const accounts = await window.ethereum.request({
           method: "eth_requestAccounts",
         });
         const account = accounts[0];
         setAccount(account);
-        console.log(account);
+        /*  console.log(account); */
 
+        // Fetch the balance of the connected account
         const balanceResult = await window.ethereum.request({
           method: "eth_getBalance",
-          params: [account, "latest"],
+          params: [account, "latest"], //"latest" second paramter to make sure the api retrieved the balance from the lastest block of blockchain
         });
 
-        const wei = parseInt(balanceResult, 16);
-        const balance = wei / 10 ** 18;
+        //The returned balance is in hex.
+
+        const wei = parseInt(balanceResult, 16); // convert 'hex' to 'decimal number'
+
+        const balance = wei / 10 ** 18; //Convert wei to ETH
+
         setBalance(balance.toFixed(6));
 
         /* console.log(balance); */
       } catch (error) {
         console.error("Error connecting to Metamask:", error);
+        alert("Error connecting to Metamask: " + error.message);
       }
     } else {
       console.error("Metamask not detected");
+      alert(
+        "Metamask not detected. Please install Metamask to use this feature."
+      );
     }
   };
 
